5bd3df239776b985a1bd76043bfef8b9b52c866d
awana <80144987+awana111@users.noreply.github.com>
2025-05-17 13:57:46 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_X00TD.mk
+    $(LOCAL_DIR)/corvus_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_X00TD-user \
-    spark_X00TD-userdebug \
-    spark_X00TD-eng
+    corvus_X00TD-user \
+    corvus_X00TD-userdebug \
+    corvus_X00TD-eng
