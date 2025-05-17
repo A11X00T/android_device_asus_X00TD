ecb0bf78555622bdb3fcb646bbe3943868556340
awana <80144987+awana111@users.noreply.github.com>
2025-05-17 13:53:16 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,12 @@ rm -rf hardware/qcom-caf/msm8998/display
 git clone https://github.com/pkm774/hardware_qcom-caf_display_msm8998 -b 11 hardware/qcom-caf/msm8998/display
 
 rm -rf devi*/a*/X*/vendors*
+
+rm -rf vendor/asus
+git clone https://github.com/A11X00T/android_vendor_asus_X00TD -b eleven vendor/asus
+
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --single-branch https://github.com/texascake/android_kernel_asus_sdm660-4.4 -b lineage-18.1 kernel/asus/sdm660
+
+#rm -rf vendor/extra
+git clone https://github.com/texascake/vendor kunci && cp -R kunci/* vendor/ && rm -rf kunci
